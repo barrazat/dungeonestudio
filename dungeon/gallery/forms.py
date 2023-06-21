68bd86ee7a9ffe01eb1c6bd6907bcf1fe00145c1
Leonardo Barraza <leobarrazat@gmail.com>
2023-06-21 18:44:46 -0400

    add create song template

--- dungeon/gallery/forms.py
+++ dungeon/gallery/forms.py
@@ -6,9 +6,30 @@ from django.db.models import fields
 from django.forms import widgets
 from django.forms.widgets import DateTimeInput
 from datetime import date
-from .models import Artist
+from .models import Artist, Song, Tracklist, Album
 
 class ArtistForm(forms.ModelForm):
+    name = forms.CharField(min_length=5, max_length=50, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre del Artista'}))
+    url = forms.URLField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Url de Spotify, YouTube, etc.'}))
     class Meta:
         model = Artist
-        fields = '__all__'
+        fields = ['name', 'url']
+
+class SongForm(forms.ModelForm):
+    CHOICES_GENRE = (
+        (0, "Trap"),
+        (1, "R&B"),
+        (2, "Cyber"),
+        (3, "Reggaeton"),
+        (4, "Drill"),
+        (5, "Plugg"),
+        (6, "Pluggnb"),
+        (7, "Hard")
+    )
+
+    name = forms.CharField(min_length=5, max_length=50, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre de la Canción'}))
+    url = forms.URLField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Url de Spotify, YouTube, etc.'}))
+    genre = forms.ChoiceField(choices=CHOICES_GENRE)
+    class Meta:
+        model = Song
+        fields = ['name', 'url', 'beat', 'mix', 'master', 'release_date', 'genre', 'cover_art', 'id_artist']
--- dungeon/gallery/views.py
+++ dungeon/gallery/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Artist, Song, Album, Tracklist
-# Create your views here.
+from .forms import ArtistForm, SongForm
+from django.contrib.auth.decorators import permission_required
+from django.contrib import messages
+
 def home(request):
     songs = Song.objects.all()
     tracklist = Tracklist.objects.all()
@@ -9,3 +12,18 @@ def home(request):
         'tracklist': tracklist
     }
     return render(request, 'home.html', data)
+
+@permission_required('app.add_producto')
+def create_song(request):
+    data = {
+        'form': SongForm()
+    }
+
+    if request.method == 'POST':
+        formulario = SongForm(data=request.POST, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            messages.success(request, "Canción guardada con exito.")
+        else:
+            data["form"] = formulario       
+    return render(request, 'song/create.html', data)
